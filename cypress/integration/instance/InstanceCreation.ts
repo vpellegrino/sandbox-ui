6f01f6dba84b0ec206b80f7514aa679a25bc90d1
Sona Mala <smala@redhat.com>
2022-11-15 14:18:20 +0100

    MGDOBR-1178: an isolation of the click on the Creating button

--- cypress/integration/instance/InstanceCreation.ts
+++ cypress/integration/instance/InstanceCreation.ts
@@ -23,77 +23,16 @@ describe("the 'Create a SE instance' Modal", () => {
   });
 
   it("Submit", () => {
-    createInstance(newInstanceName);
-
-    //Assert that the process of the instnace's creation was started
-    cy.ouiaId("Instances list table", "PF4/Table")
-      .ouiaId(newInstanceName, "PF4/TableRow")
-      .should("be.visible")
-      .within(() => {
-        //The user cannot go to the instance's details if it is not ready
-        //Assert that the inner text contains only the name of the instance and no <a>
-        cy.get("td:first").should("have.html", newInstanceName);
-        //The status column is loading and contains ready
-        cy.get("td:nth-child(3)")
-          .ouiaId("accepted", "QE/ResourceStatus")
-          .within(() => {
-            cy.get("span[role='progressbar']").should("exist");
-            cy.ouiaId("creating", "PF4/Button").should("have.text", "Creating");
-            cy.ouiaId("ready-shortly", "QE/HelperTextItem").should(
-              "have.text",
-              "This will be ready shortly."
-            );
-          });
-        cy.ouiaId("creating", "PF4/Button").click();
-      });
-
-    //Assert that the process of the instnace's creation is monitored
-    cy.ouiaId("se-status", "QE/Popover")
-      .should("be.visible")
-      .within(() => {
-        cy.ouiaType("QE/StackItem").should("have.length", "3");
-        cy.ouiaId("info-banner", "QE/StackItem")
-          .ouiaId("ready-shortly", "PF4/Text")
-          .should("be.visible");
-
-        cy.ouiaId("steps-count", "QE/StackItem").should(
-          "have.text",
-          "0 of 3 steps completed"
-        );
-        progressStepsStatuses(SEInstanceStatus.ACCEPTED);
-
-        //The first steps takes about 65 secs.
-        cy.ouiaId("steps-count", "QE/StackItem", { timeout: 90000 }).should(
-          "have.text",
-          "1 of 3 steps completed"
-        );
-        progressStepsStatuses(SEInstanceStatus.PREPARING);
-
-        if (isEnvironmentType(EnvType.Mocked)) {
-          //Preparing -> Ready state takes 9 secs.
-          //The Popover disappears on Dev when we assert this element.
-          cy.ouiaId("steps-count", "QE/StackItem", { timeout: 10000 }).should(
-            "have.text",
-            "2 of 3 steps completed"
-          );
-          progressStepsStatuses(SEInstanceStatus.PROVISIONING);
-        }
-      });
-    cy.ouiaId("se-status", "QE/Popover", { timeout: 60000 }).should(
-      "not.exist"
-    );
-
-    //Assertation that the instance was created
-    cy.ouiaId("Instances list table", "PF4/Table")
-      .ouiaId(newInstanceName, "PF4/TableRow")
-      .within(() => {
-        cy.get("td:first a[data-testid='tableInstances-linkInstance']").should(
-          "be.visible"
-        );
-        cy.ouiaId("ready", "QE/ResourceStatus")
-          .should("be.visible")
-          .should("have.text", "Ready");
-      });
+    //forgot the current test
+    //I have set the selector by the different way to be sure that the problem is not in our ouiaId command
+    //I am working with Instance three to be sure that there is no update and I have enough time to click on it
+    cy.get(
+      "[data-ouia-component-id='Instances list table'][data-ouia-component-type='PF4/Table'] [data-ouia-component-id='Instance three'][data-ouia-component-type='PF4/TableRow'] [data-ouia-component-id='creating'][data-ouia-component-type='PF4/Button']"
+    )
+      //assert that the selector exists - in Cypress UI I checked that it marks the right button
+      .should("exist")
+      //does not work - why???
+      .click();
   });
 
   // instance with name "error-test" already exists
@@ -116,7 +55,7 @@ describe("the 'Create a SE instance' Modal", () => {
 
   onlyOn(isEnvironmentType(EnvType.Mocked), () => {
     // instance with name containing "error-test" causes 4xx response code
-    it("Submit and expect error while creating", () => {
+    it.skip("Submit and expect error while creating", () => {
       const errorInstanceName: string = "error-test-2";
       cy.ouiaId("create-smart-event-instance", "PF4/Button").click();
       cy.ouiaId("create-instance", "PF4/ModalContent").then(($modal) => {
@@ -133,7 +72,7 @@ describe("the 'Create a SE instance' Modal", () => {
       });
     });
 
-    it("Submit and expect quota error", () => {
+    it.skip("Submit and expect quota error", () => {
       const errorInstanceName: string = "quota-error";
       cy.ouiaId("create-smart-event-instance", "PF4/Button").click();
       cy.ouiaId("create-instance", "PF4/ModalContent").then(($modal) => {
@@ -151,7 +90,7 @@ describe("the 'Create a SE instance' Modal", () => {
     });
   });
 
-  it("Cancel", () => {
+  it.skip("Cancel", () => {
     const canceledInstanceName: string = uniqueName("canceled-instance");
     createInstance(canceledInstanceName, "cancel");
 
@@ -165,7 +104,7 @@ describe("the 'Create a SE instance' Modal", () => {
      * The best practices does not recommand the "after" metod for UI.
      * Cypress guaranted the order of the test execution.
      */
-    it("Clean the new instance", () => {
+    it.skip("Clean the new instance", () => {
       deleteInstance(newInstanceName);
       cy.ouiaId("Instances list table", "PF4/Table").within(() => {
         // once delete confirmed, state should change
